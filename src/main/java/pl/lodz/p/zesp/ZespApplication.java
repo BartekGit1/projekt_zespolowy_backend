83c678b268fe0bd5b45e857004626bb89822b787
BartekGit1 <clerniseq@gmail.com>
2025-04-14 22:40:52 +0200

    initial commit

---
+++ src/main/java/pl/lodz/p/zesp/ZespApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.zesp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZespApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZespApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/lodz/p/zesp/ZespApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.zesp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZespApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
