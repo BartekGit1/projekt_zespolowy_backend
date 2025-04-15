bd4b139588fc887ade369df19699e6c89494ae0a
BartekGit1 <clerniseq@gmail.com>
2025-04-15 22:28:57 +0200

    added database structure

--- src/main/resources/db/migration/V1.0__Init.sql
+++ src/main/resources/db/migration/V1.0__Init.sql
@@ -10,3 +10,84 @@ CREATE TABLE users
 INSERT INTO users (username, email, account_status, role)
 VALUES ('mariasilva', 'bartoszzuchora@o2.pl', 'ACTIVE', 'CUSTOMER'),
        ('johndoe', 'clerniseq@gmail.com', 'ACTIVE', 'ADMIN');
+
+CREATE TABLE auctions
+(
+    id             BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    version        bigint         not null,
+    title          VARCHAR(255)   NOT NULL,
+    description    TEXT           NOT NULL,
+    starting_price DECIMAL(10, 2) NOT NULL,
+    current_price  DECIMAL(10, 2) NOT NULL,
+    end_date       TIMESTAMP      NOT NULL,
+    user_id        BIGINT         NOT NULL,
+    is_promoted    BOOLEAN        NOT NULL DEFAULT FALSE,
+    created_at     TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    finished       boolean        NOT NULL,
+    paid           BOOLEAN        NOT NULL DEFAULT FALSE,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE bids
+(
+    id         BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    auction_id BIGINT         NOT NULL,
+    user_id    BIGINT         NOT NULL,
+    amount     DECIMAL(10, 2) NOT NULL,
+    bid_time   TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (auction_id) REFERENCES auctions (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE premium_subscriptions
+(
+    id         BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id    BIGINT       NOT NULL,
+    start_date TIMESTAMP    NOT NULL,
+    end_date   TIMESTAMP    NOT NULL,
+    payment_id VARCHAR(100) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE watchlist
+(
+    user_id    BIGINT NOT NULL,
+    auction_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, auction_id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (auction_id) REFERENCES auctions (id)
+);
+
+CREATE TABLE auto_bids
+(
+    id         BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id    BIGINT         NOT NULL,
+    auction_id BIGINT         NOT NULL,
+    max_amount DECIMAL(10, 2) NOT NULL,
+    created_at TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    active     BOOLEAN        NOT NULL DEFAULT TRUE,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (auction_id) REFERENCES auctions (id)
+);
+
+CREATE TABLE payments
+(
+    id         BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id    BIGINT         NOT NULL,
+    amount     DECIMAL(10, 2) NOT NULL,
+    status     VARCHAR(20)    NOT NULL,
+    created_at TIMESTAMP      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
+
+CREATE TABLE auction_payments
+(
+    id         BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    auction_id BIGINT    NOT NULL,
+    user_id    BIGINT    NOT NULL,
+    payment_id BIGINT    NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (auction_id) REFERENCES auctions (id),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (payment_id) REFERENCES payments (id)
+);
