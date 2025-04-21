544b73afd3e061e4088de2b656ae8a08428e2696
Piotr Pabich <87279370+PiotrPabichCode@users.noreply.github.com>
2025-04-21 11:56:08 +0200

    Add Swagger

--- src/main/java/pl/lodz/p/zesp/common/util/config/SecurityConfig.java
+++ src/main/java/pl/lodz/p/zesp/common/util/config/SecurityConfig.java
@@ -4,6 +4,7 @@ package pl.lodz.p.zesp.common.util.config;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,6 +12,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.Arrays;
 
 import static pl.lodz.p.zesp.common.util.Constants.*;
 
@@ -26,6 +30,20 @@ class SecurityConfig {
         this.jwtAuthConverter = jwtAuthConverter;
     }
 
+    public static final AntPathRequestMatcher[] WHITELIST_URLS = {
+            // Swagger/OpenAPI
+            new AntPathRequestMatcher("/v3/api-docs/**"),
+            new AntPathRequestMatcher("/api-docs"),
+            new AntPathRequestMatcher("/api-docs/**"),
+            new AntPathRequestMatcher("/swagger-resources"),
+            new AntPathRequestMatcher("/swagger-resources/**"),
+            new AntPathRequestMatcher("/configuration/ui"),
+            new AntPathRequestMatcher("/configuration/security"),
+            new AntPathRequestMatcher("/swagger-ui.html"),
+            new AntPathRequestMatcher("/webjars/**"),
+            new AntPathRequestMatcher("/swagger-ui/**")
+    };
+
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.cors(Customizer.withDefaults());
@@ -33,8 +51,9 @@ class SecurityConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authz -> authz.
                         requestMatchers(URI_VERSION_V1 + URI_USERS + URI_REGISTER)
-                        .permitAll().
-                        anyRequest().authenticated()
+                        .permitAll()
+                        .requestMatchers(WHITELIST_URLS).permitAll()
+                        .anyRequest().authenticated()
                 )
                 .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 ).exceptionHandling(exceptionHandling -> exceptionHandling
